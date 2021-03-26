396d692ee8c1d0715603664131a9c05522f77d3c
Jaeyoon Song <jyo3on@gmail.com>
2021-03-26 13:46:26 -0400

    timeout

--- client/game/Round.jsx
+++ client/game/Round.jsx
@@ -3,14 +3,55 @@ import React from "react";
 import PlayerProfile from "./PlayerProfile.jsx";
 import Task from "./Task.jsx";
 import About from "../intro/About.jsx";
+import {
+  Callout, Intent,
+} from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
 
 export default class Round extends React.Component {
+  timeout = false;
+  t1; t2;
+
   render() {
     const { game, round, stage, player } = this.props;
 
+    if (this.timeout && !(player.idle || !player.online)) {
+      clearTimeout(this.t1);
+      clearTimeout(this.t2);
+      console.log("timeout cleared");
+      this.timeout = false;
+    }
+    else if (!this.timeout && (player.idle || !player.online)) {
+      this.timeout = true;
+      console.log("timeout set")
+
+      this.t1 = setTimeout(() => {
+        alert("If you are idle or offline for more than 3 minutes, the task will be cancelled.")
+      }, 1 * 60 * 1000);
+
+      this.t2 = setTimeout(() => {
+        if (player.idle || !player.online) {
+          localStorage.setItem("confirmed", "");
+          player.exit("Thanks")
+        }
+      }, 15 * 60 * 1000);
+    }
+
     return (
       <div className="round">
-        <About/>
+
+        {player.idle || !player.online?
+            <Callout
+              icon={IconNames.ERROR}
+              intent={Intent.DANGER}
+              title="Warning"
+            >
+              If you are idle or offline for more than 3 minutes, the task will be cancelled.
+            </Callout>
+          :
+          ""
+        }
+        <About {...this.props}/>
         <div className="content">
           <PlayerProfile player={player} stage={stage} game={game} />
           <Task game={game} round={round} stage={stage} player={player} />
