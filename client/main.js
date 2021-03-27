2108c58433bbd38babc8eea8649520138abeef2f
Jaeyoon Song <jyo3on@gmail.com>
2021-03-27 18:33:22 -0400

    add quiz for quality control

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import Empirica from "meteor/empirica:core";
 import { render } from "react-dom";
 import ExitSurvey from "./exit/ExitSurvey";
 import Thanks from "./exit/Thanks";
-import About from "./intro/About";
+import Quiz from "./intro/Quiz";
 import Round from "./game/Round";
 import Instruction from "./intro/Instruction";
 import Identification from "./intro/Identification";
@@ -19,7 +19,7 @@ Empirica.newPlayer(Identification);
 // At this point they have been assigned a treatment. You can return
 // different instruction steps depending on the assigned treatment.
 Empirica.introSteps((game, treatment) => {
-  const steps = [Instruction];
+  const steps = [Instruction, Quiz];
   return steps;
 });
 
