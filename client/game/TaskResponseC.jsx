93c78966c69c03a6d8aa9fb43f63f14743484562
Hao <78881332+haoranwen0@users.noreply.github.com>
2021-04-26 00:07:14 -0400

    use this draft form edit
    had the original draft stayed at the top and rendered the textarea beneath it

--- client/game/TaskResponseC.jsx
+++ client/game/TaskResponseC.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-import {
-  Callout, Intent,
-} from "@blueprintjs/core";
+import { Callout, Intent } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
 
 export default class TaskResponseC extends React.Component {
@@ -19,95 +17,99 @@ export default class TaskResponseC extends React.Component {
     };
   }
 
-  
-
   handleSelect = (id) => {
     if (id >= 0) {
-        this.setState(prevState => ({
-            ...prevState,
-            selected: id,
-        }));
+      this.setState((prevState) => ({
+        ...prevState,
+        selected: id,
+      }));
     }
-    
-  }
+  };
 
   handleSkip = () => {
-      const { selected } = this.state;
-      const { round } = this.props;
-      const drafts = round.data.drafts;
-      let newSelected = selected + 1;
+    const { selected } = this.state;
+    const { round } = this.props;
+    const drafts = round.data.drafts;
+    let newSelected = selected + 1;
 
-      if (selected >= 0 && selected < drafts.length - 1) {
-        this.setState(prevState => ({
-            ...prevState,
-            selected: newSelected,
-        }));
-      }
-      else {
-        this.setState(prevState => ({
-            ...prevState,
-            selected: 0,
-        }));
-      }
-  }
+    if (selected >= 0 && selected < drafts.length - 1) {
+      this.setState((prevState) => ({
+        ...prevState,
+        selected: newSelected,
+      }));
+    } else {
+      this.setState((prevState) => ({
+        ...prevState,
+        selected: 0,
+      }));
+    }
+  };
 
   handleConfirm = () => {
     const { selected, stories } = this.state;
     const { round, player } = this.props;
     const drafts = round.data.drafts;
-    
-    const newStories = [...stories, drafts[selected].content]
 
-    console.log(newStories)
+    // const newStories = [...stories, drafts[selected].content];
+    const newStories = [""];
+
+    console.log(newStories);
 
     if (selected >= 0) {
-        this.setState(prevState => ({
-            ...prevState,
-            confirmed: true,
-            stories: newStories,
-        }), () => {
+      this.setState(
+        (prevState) => ({
+          ...prevState,
+          confirmed: true,
+          stories: newStories,
+        }),
+        () => {
           player.round.set("value", newStories);
           localStorage.setItem("confirmed", true);
-        });
+        }
+      );
     }
-    
-  }
+  };
 
   handleBack = () => {
     const { stories } = this.state;
     const { player } = this.props;
 
-    const newStories = stories.filter((s, i) => i != stories.length - 1)
+    const newStories = stories.filter((s, i) => i != stories.length - 1);
     console.log(newStories);
 
-    this.setState(prevState => ({
+    this.setState(
+      (prevState) => ({
         ...prevState,
         confirmed: false,
         stories: newStories,
-    }), () => {
-      player.round.set("value", newStories);
-    });
+      }),
+      () => {
+        player.round.set("value", newStories);
+      }
+    );
 
     localStorage.setItem("confirmed", "");
-    
-  }
+  };
 
   handleChange = (e) => {
     const { stories } = this.state;
     const { player } = this.props;
-    const value = e.target.value
+    const value = e.target.value;
 
     const newNum = this.countWords(value);
     const newStories = [...stories];
     newStories[stories.length - 1] = value;
 
-    this.setState(prevState => ({
+    this.setState(
+      (prevState) => ({
         ...prevState,
         numOfWords: newNum,
         stories: newStories,
-    }), () => {
-      player.round.set("value", newStories);
-    });
+      }),
+      () => {
+        player.round.set("value", newStories);
+      }
+    );
   };
 
   handleNext = (e) => {
@@ -116,38 +118,39 @@ export default class TaskResponseC extends React.Component {
     const { numOfWords, stories } = this.state;
     const { player, round } = this.props;
 
-    if (numOfWords < 200)
-      alert("The story is less than 200 words.");
+    if (numOfWords < 200) alert("The story is less than 200 words.");
     else {
       localStorage.setItem("confirmed", "");
 
       // show drafts again
       // add story to state array
-      const newStories = [...stories]
+      const newStories = [...stories];
       console.log(newStories);
 
-      this.setState(prevState => ({
-        ...prevState,
-        confirmed: false,
-        stories: newStories,
-        submitted: true,
-        numOfWords: 0,
-      }), () => {
-        setTimeout(() => {
-          if (this.state.submitted) {
-            this.setState(prevState => ({
-              ...prevState,
-              submitted: false,
-            }))
-          }
-        }, 4000)
-      });
+      this.setState(
+        (prevState) => ({
+          ...prevState,
+          confirmed: false,
+          stories: newStories,
+          submitted: true,
+          numOfWords: 0,
+        }),
+        () => {
+          setTimeout(() => {
+            if (this.state.submitted) {
+              this.setState((prevState) => ({
+                ...prevState,
+                submitted: false,
+              }));
+            }
+          }, 4000);
+        }
+      );
 
       player.round.set("value", newStories);
       this.props.handleScore(newStories.length, round.index);
-
     }
-  }
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -157,27 +160,32 @@ export default class TaskResponseC extends React.Component {
   };
 
   countWords = (str) => {
-    const result = str.trim().split(/\s+/)
-    if (result == "")
-      return 0;
+    const result = str.trim().split(/\s+/);
+    if (result == "") return 0;
     return result.length;
-  }
+  };
 
   renderDraft = (draft) => {
     const { selected } = this.state;
 
     let classes = "draft";
-    if (selected == draft.id)  
-        classes = "draft selected";
-    
+    if (selected == draft.id) classes = "draft selected";
+
     return (
-        <div className={classes} key={draft.id}>
-            <b>Draft {draft.id + 1}</b> &nbsp;
-            {draft.content.substring(0,100)}...<br/>
-            {selected == draft.id ? "" : <button className="btn" onClick={() => this.handleSelect(draft.id)}>View this draft</button>}
-        </div>
-    )
-  }
+      <div className={classes} key={draft.id}>
+        <b>Draft {draft.id + 1}</b> &nbsp;
+        {draft.content.substring(0, 100)}...
+        <br />
+        {selected == draft.id ? (
+          ""
+        ) : (
+          <button className="btn" onClick={() => this.handleSelect(draft.id)}>
+            View this draft
+          </button>
+        )}
+      </div>
+    );
+  };
 
   renderSubmitted() {
     return (
@@ -191,7 +199,7 @@ export default class TaskResponseC extends React.Component {
 
   renderTextarea(story, index) {
     const { stories } = this.state;
-    const isActive = (stories.length - 1 == index) ? "active " : "";
+    const isActive = stories.length - 1 == index ? "active " : "";
 
     return (
       <textarea
@@ -199,8 +207,7 @@ export default class TaskResponseC extends React.Component {
         value={story}
         key={index}
         className={isActive + "story story" + index}
-      >
-      </textarea>
+      ></textarea>
     );
   }
 
@@ -214,22 +221,23 @@ export default class TaskResponseC extends React.Component {
       console.log(game.get("score"));
 
       if (!currentValue || currentValue.length == 0) {
-        this.setState(prevState => ({
+        this.setState((prevState) => ({
           ...prevState,
           stories: [],
         }));
-      }
-      else {
-        this.setState(prevState => ({
+      } else {
+        this.setState((prevState) => ({
           ...prevState,
           stories: currentValue,
         }));
       }
     }
-    
+
     if (document.querySelector(".story.active")) {
-      const newNum = this.countWords(document.querySelector(".story.active").value)
-      this.setState(prevState => ({
+      const newNum = this.countWords(
+        document.querySelector(".story.active").value
+      );
+      this.setState((prevState) => ({
         ...prevState,
         numOfWords: newNum,
       }));
@@ -252,45 +260,56 @@ export default class TaskResponseC extends React.Component {
 
     return (
       <div className="task-response">
-        {
-          submitted ? 
-            <div className="success">
-              <Callout
-                icon={IconNames.TICK}
-                intent={Intent.SUCCESS}
-                title="Success"
-              >
-                Successfully submitted. Thank you so much. Enjoy writing your next story!
-              </Callout>
-            </div>
-          :
+        {submitted ? (
+          <div className="success">
+            <Callout
+              icon={IconNames.TICK}
+              intent={Intent.SUCCESS}
+              title="Success"
+            >
+              Successfully submitted. Thank you so much. Enjoy writing your next
+              story!
+            </Callout>
+          </div>
+        ) : (
           ""
-        }
-        {confirmed ? 
-            <form className="task-response-form" onSubmit={this.handleSubmit}>
-
-                {stories.map((s, i) => this.renderTextarea(s, i))}
-
-                <div>Total {numOfWords} words</div>
-                <button ref={this.submitRef} className="hidden" type="submit">Submit</button>
-                <button className="green" onClick={this.handleNext}>Submit (Start writing next story)</button>
-                <button className="orange" onClick={this.handleBack}>Go back to choose a different draft</button>
-            </form>
-            :
+        )}
+        {confirmed ? (
+          <div style={{ width: "70%" }}>
             <div className="task-response-form">
-                <div className="selected-draft">
-                    {drafts[selected].content}
-                </div>
-                <button className="green" onClick={this.handleConfirm}>Use this draft</button>
-                <button className="orange" onClick={this.handleSkip}>Skip this draft</button>
+              <div className="selected-draft">{drafts[selected].content}</div>
             </div>
-        }
+            <form className="task-response-form" onSubmit={this.handleSubmit}>
+              {stories.map((s, i) => this.renderTextarea(s, i))}
+
+              <div>Total {numOfWords} words</div>
+              <button ref={this.submitRef} className="hidden" type="submit">
+                Submit
+              </button>
+              <button className="green" onClick={this.handleNext}>
+                Submit (Start writing next story)
+              </button>
+              <button className="orange" onClick={this.handleBack}>
+                Go back to choose a different draft
+              </button>
+            </form>
+          </div>
+        ) : (
+          <div className="task-response-form">
+            <div className="selected-draft">{drafts[selected].content}</div>
+            <button className="green" onClick={this.handleConfirm}>
+              Use this draft
+            </button>
+            <button className="orange" onClick={this.handleSkip}>
+              Skip this draft
+            </button>
+          </div>
+        )}
 
         <div className={confirmed ? "archive confirmed" : "archive"}>
-            <h4>All drafts</h4>
-            {drafts.map(this.renderDraft)}
+          <h4>All drafts</h4>
+          {drafts.map(this.renderDraft)}
         </div>
-        
       </div>
     );
   }
