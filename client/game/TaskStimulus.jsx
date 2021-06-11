c2d2ff8f3dec6e987f338f79864b561fa285ff9b
Raza <razaabbastl@gmail.com>
2021-06-10 17:43:07 -0700

    instruction fix

--- client/game/TaskStimulus.jsx
+++ client/game/TaskStimulus.jsx
@@ -12,8 +12,6 @@ export default class TaskStimulus extends React.Component {
           <div>
             <b>Please modify the provided function to achieve the indicated functionality and submit the code in the window on the right.</b>
             <br />
-            You have up to 30 minutes.
-            <br />
             You can refer to the instructions by clicking on the button on the
             top right corner. If the task description does not appear at first, try refreshing the page.
           </div>
